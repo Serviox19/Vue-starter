969fdf90de2b84df40b1f15fe594b4e727f40b38
Servio <servio.mora19@gmail.com>
2019-11-09 11:59:58 -0500

    set up view and ading components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>App</h1>
+    <SearchBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px
 }
 </style>
